979f63262df65d2bbc40fdaa586a48107eb4a0c3
ribeirolivia <ribeiro.livias@hotmail.com>
2021-12-06 00:06:32 -0300

    Add theme dark/light

--- src/App.js
+++ src/App.js
@@ -1,13 +1,56 @@
+import React, { Fragment , useState , useEffect  } from "react";
+
+import { ThemeProvider } from "styled-components";
+import { lightTheme , darkTheme } from "./theme";
+import GlobalTheme from "./GlobalStyle";
+import styled from "styled-components";
 import Navbar from "./components/Navbar/Navbar";
-import Teste from './components/teste/Teste'
+
+
+
+
+const ButtonChange = styled.button`
+  width: 100px;
+  height: 40px;
+  margin-right: 20px;
+  border-radius: 10px;
+`;
 
 function App() {
+
+  const [theme, setTheme] = useState('dark');
+
+  const toggleTheme = () => {
+    if (theme === 'light') {
+      window.localStorage.setItem('theme', 'dark');
+      setTheme('dark');
+    }else {
+      window.localStorage.setItem('theme', 'light');
+      setTheme('ligth')
+    }
+  };
+  useEffect(() => {
+    const localTheme = window.localStorage.getItem('theme');
+    localTheme && setTheme(localTheme);
+  }, [])
+
+
   return (
-    <div className="App">
-      <Navbar/>
-      <Teste/>
-    </div>
+    
+      <ThemeProvider theme={ theme ==='light' ? lightTheme : darkTheme}>
+        <Fragment>
+          <GlobalTheme>
+            
+              <ButtonChange onClick={toggleTheme} >trocar tema </ButtonChange>
+              
+              <Navbar/>
+            
+          </GlobalTheme>
+        </Fragment>
+      </ThemeProvider>
+    
   );
 }
+
 
 export default App;
