11210b25f5bea151ec2411a007b19c157f706d0d
ribeirolivia <ribeiro.livias@hotmail.com>
2021-12-23 21:52:59 -0300

    Style card produtos, page produtos. Ajuste no scroll horizontal e tamanho da pagina

--- src/Pages/Menu.js
+++ src/Pages/Menu.js
@@ -4,55 +4,55 @@ import styled from "styled-components";
 
 const MenuCards = styled.div`
   margin-top: 0.5rem;
-  display:flex;
+  display: flex;
   flex-wrap: wrap;
-    
 `;
 const MenuItens = styled.div`
-width:100%;
-
-`
+  width: 100%;
+`;
 
 const CardsMenu = styled.a`
-text-decoration: none;
-
-`
+  text-decoration: none;
+`;
 
 const Menu = () => {
   return (
     <MenuItens>
       <MenuCards>
-        
         <CardsMenu href="/category">
-          <CardItem 
-          img="https://fotos.vivadecora.com.br/decoracao-loja-de-artigos-esportivos-vanessalora-173250-square_cover_xsmall.jpg" 
-          name="Categoria"/>
+          <CardItem
+            img="https://fotos.vivadecora.com.br/decoracao-loja-de-artigos-esportivos-vanessalora-173250-square_cover_xsmall.jpg"
+            name="Categoria"
+          />
         </CardsMenu>
 
         <CardsMenu href="/Product">
-          <CardItem 
-          img="https://fotos.vivadecora.com.br/decoracao-loja-de-artigos-esportivos-vanessalora-173250-square_cover_xsmall.jpg" 
-          name="Ofertas" />
+          <CardItem
+            img="https://fotos.vivadecora.com.br/decoracao-loja-de-artigos-esportivos-vanessalora-173250-square_cover_xsmall.jpg"
+            name="Ofertas"
+          />
         </CardsMenu>
 
         <CardsMenu href="/">
-          <CardItem 
-          img="https://fotos.vivadecora.com.br/decoracao-loja-de-artigos-esportivos-vanessalora-173250-square_cover_xsmall.jpg" 
-          name="Minha conta" />
+          <CardItem
+            img="https://fotos.vivadecora.com.br/decoracao-loja-de-artigos-esportivos-vanessalora-173250-square_cover_xsmall.jpg"
+            name="Minha conta"
+          />
         </CardsMenu>
 
         <CardsMenu href="/product">
-          <CardItem 
-          img="https://fotos.vivadecora.com.br/decoracao-loja-de-artigos-esportivos-vanessalora-173250-square_cover_xsmall.jpg" 
-          name="Produtos" />
+          <CardItem
+            img="https://fotos.vivadecora.com.br/decoracao-loja-de-artigos-esportivos-vanessalora-173250-square_cover_xsmall.jpg"
+            name="Produtos"
+          />
         </CardsMenu>
 
         <CardsMenu href="/Store">
-          <CardItem 
-          img="https://fotos.vivadecora.com.br/decoracao-loja-de-artigos-esportivos-vanessalora-173250-square_cover_xsmall.jpg" 
-          name="Lojas" />
+          <CardItem
+            img="https://fotos.vivadecora.com.br/decoracao-loja-de-artigos-esportivos-vanessalora-173250-square_cover_xsmall.jpg"
+            name="Lojas"
+          />
         </CardsMenu>
-
       </MenuCards>
     </MenuItens>
   );
