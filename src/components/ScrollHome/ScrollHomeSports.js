7d232205b06596617833d0e8beb788d18e2e3716
ribeirolivia <ribeiro.livias@hotmail.com>
2021-12-30 20:19:12 -0300

    Add GlobalStyle, scrollHome-store

--- src/components/ScrollHome/ScrollHomeSports.js
+++ src/components/ScrollHome/ScrollHomeSports.js
@@ -1,63 +1,65 @@
 import React from "react";
 import HorizontalScroll from "react-scroll-horizontal";
 import axios from "axios";
-import { useState, useEffect } from 'react';
-
+import { useState, useEffect } from "react";
 
 import CardItem from "../Cards/CardItem";
 import styled from "styled-components";
 
-
-
 const CardGroup = styled.div`
   width: 100%;
-  height: 10rem;
-  padding: 0.5rem;
+  height: 7rem;
+  padding: 0.6rem;
 `;
 const CardCategory = styled.div`
-  display:flex;
-  
+  display: flex;
 `;
 
 const ScrollHomeSports = () => {
-
-
   const [categoryCard, setCategoryCard] = useState([]);
   const [mounted, setMounted] = useState(false);
 
-  const getData = async () => {
-    await axios.get('/categories/find-all')
-    .then(response => {
-      if(mounted) {
+  const [storeCard, setStoreCard] = useState([]);
+
+  const getDataCategories = async () => {
+    await axios.get("/categories/find-all").then((response) => {
+      if (mounted) {
         setCategoryCard(response.data);
+      }
+    });
+  };
+
+  const getDataStore = async () => {
+    await axios.get("/companies/find-all").then((response) => {
+      if (mounted) {
+        setStoreCard(response.data);
       }
     });
   };
 
   useEffect(() => {
     setMounted(true);
-    getData();
+    getDataCategories();
+    getDataStore();
     // eslint-disable-next-line
-  },[mounted]);
-  
-
+  }, [mounted]);
 
   return (
     <CardGroup>
+      <HorizontalScroll>
+        <CardCategory>
+          {categoryCard.map(category => (
+            <CardItem img={category.image} name={category.name} />
+          ))}
+        </CardCategory>
+      </HorizontalScroll>
 
       <HorizontalScroll>
-
         <CardCategory>
-          {
-            categoryCard.map( category => (
-          <CardItem
-            img={category.image}
-            name={category.name}
-          />
-          ))
-          }
+          {storeCard.map(store => (
+            <CardItem img={store.image} name={store.name} />
+          ))}
         </CardCategory>
-        
       </HorizontalScroll>
     </CardGroup>
   );
